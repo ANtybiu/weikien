b469396601d981c119d4bcce416c88761fb57dec
ANtybiu <88878367+ANtybiu@users.noreply.github.com>
2025-09-01 10:26:03 +0800

    Update super_heavy.js

--- super_heavy.js
+++ super_heavy.js
@@ -84,5 +84,66 @@ targetAll.forEach(target5 => {
       }, 16); 
     }
 
+    let serviceIndex = 1;
+
+    function nextService(){
+      if(serviceIndex!==6){
+      serviceIndex++;
+        updateService();
+        document.getElementById("service-progress").innerHTML = `${serviceIndex}/6`
+      }
+    }
+    function prevService(){
+      if(serviceIndex!==1){
+      serviceIndex--;
+        updateService();
+         document.getElementById("service-progress").innerHTML = `${serviceIndex}/6`
+      }
+    }
+    function updateService(){
+      if(serviceIndex === 1){
+        document.querySelector(".gat1").innerHTML = `<img src="logo.png" id="service-logo">
+          <span id="h1b">Consultaion</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+        document.querySelector(".gat2").innerHTML = ` <img src="logo.png" id="service-logo">
+          <span id="h1b">Scaling & Polishing</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+      } else if(serviceIndex === 2){
+        document.querySelector(".gat1").innerHTML = `<img src="logo.png" id="service-logo">
+          <span id="h1b">Dental Filling</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+        document.querySelector(".gat2").innerHTML = ` <img src="logo.png" id="service-logo">
+          <span id="h1b">Extraction</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+      }else if(serviceIndex === 3){
+        document.querySelector(".gat1").innerHTML = `<img src="logo.png" id="service-logo">
+          <span id="h1b">Minor Oral Surgery</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+        document.querySelector(".gat2").innerHTML = ` <img src="logo.png" id="service-logo">
+          <span id="h1b">Removable Denture</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+      }else if(serviceIndex === 4){
+        document.querySelector(".gat1").innerHTML = `<img src="logo.png" id="service-logo">
+          <span id="h1b">Crown & Bridge</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+        document.querySelector(".gat2").innerHTML = ` <img src="logo.png" id="service-logo">
+          <span id="h1b">Root Canal Treatment</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+      }else if(serviceIndex === 5){
+        document.querySelector(".gat1").innerHTML = `<img src="logo.png" id="service-logo">
+          <span id="h1b">Tooth Whitening</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+        document.querySelector(".gat2").innerHTML = ` <img src="logo.png" id="service-logo">
+          <span id="h1b">Night Guard</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+      }else if(serviceIndex === 6){
+        document.querySelector(".gat1").innerHTML = `<img src="logo.png" id="service-logo">
+          <span id="h1b">Night Guard</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+        document.querySelector(".gat2").innerHTML = ` <img src="logo.png" id="service-logo">
+          <span id="h1b">Retainer</span>
+          <pblack id="service-desc">Lorem ipsum dolor sit amet consectetur adipiscing elit.</pblack>`;
+      }
+    }
   
     
